0ba28f76ba358278edb8a7e6c377f11be6ecfc0b
Isabelly1 <isabellymartins846@gmail.com>
2022-06-26 17:09:09 -0300

    Array

---
+++ script.js
@@ -0,0 +1,28 @@
+// Crie um array que receba 5 itens e exiba no console.
+let amigos = ["Angela","Bel","Camila","Daisy","kamilly, Viniciuis"]
+console.log(amigos)
+
+// Utilize um método para adicionar um nome ao inicio do array.
+let comidas = ["Bolo", "Torta", "Batata", "Macarrão", "Banana"]
+comidas.unshift ("Maçã")
+console.log(comidas)
+
+// Utilize um método para remover o último nome do array.
+let cores = ["Rosa", "Azul", "Verde", "Preto", "Roxo"]
+cores.pop()
+console.log(cores)
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+let discentes = ["Hinata","Sakura","Sai","Kakashi","Tsunade"]
+animes.push("Naruto", "Sasuke")
+console.log(animes)
+
+// Utilize um método para remover o primeiro nome do array.
+let data = [05,10,15,20,21,25,30]
+data.shift()
+console.log(data)
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+ let numbers = [7,5,6,3,8,9,2,1,4]
+ numbers.sort()
+ console.log(numbers)
